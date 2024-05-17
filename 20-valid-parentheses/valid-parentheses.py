6941188b36c9510427a8001274f6b65b1e9bc2c8
Ganesh Thampi <ganeshthampi077@gmail.com>
2024-05-17 18:05:09 -0400

    Time: 35 ms (57.66%) | Memory: 16.6 MB (22.70%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,6 +1,8 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         res = []
+        if len(s) < 2:
+            return False
         for i in s:
             if i == "(" or i == "{" or i =="[":
                 res.append(i)
