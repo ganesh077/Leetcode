f0ef226e6c33a64a6edeb181ff0b71aebdcbd8f5
Ganesh Thampi <ganeshthampi077@gmail.com>
2024-05-21 11:58:59 -0400

    Time: 1020 ms (37.95%) | Memory: 0B (100.00%) - LeetSync

--- 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
+++ 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
@@ -1,18 +1,3 @@
-WITH MaxGrades AS (
-    SELECT student_id, MAX(grade) AS max_grade
-    FROM Enrollments
-    GROUP BY student_id
-),
-FilteredEnrollments AS (
-    SELECT e.student_id, e.course_id, e.grade
-    FROM Enrollments e
-    JOIN MaxGrades mg ON e.student_id = mg.student_id AND e.grade = mg.max_grade
-)
-SELECT student_id,
-       CASE WHEN COUNT(*) = 1 THEN course_id ELSE MIN(course_id) END AS course_id,
-       MAX(grade) AS grade
-FROM FilteredEnrollments
-GROUP BY student_id
-ORDER BY student_id;
-
-
+select student_id,course_id,grade from (select student_id,course_id,grade,
+rank() over (partition by student_id order by grade desc,course_id) as ranker from
+Enrollments )a where ranker = 1 group by  student_id order by student_id
