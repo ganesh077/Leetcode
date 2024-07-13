37daf1e83ee4af918793962dde343e879f7da264
Ganesh Thampi <ganeshthampi077@gmail.com>
2024-07-12 20:47:24 -0400

    Time: 46 ms (72.91%) | Memory: 16.6 MB (30.00%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x,y = 0,0
+
+        for i in moves:
+            if i == 'R':
+                x += 1
+            elif i == 'L':
+                x -= 1
+            elif i == 'U':
+                y += 1
+            else:
+                y -= 1
+        
+        return x == 0 and y == 0
+        
