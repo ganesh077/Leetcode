94e2db09d5db615d7f6a97ddae7ceb55ce98421a
Ganesh Thampi <ganeshthampi077@gmail.com>
2024-07-17 22:07:38 -0400

    Time: 601 ms (66.54%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,12 +1,6 @@
 # Write your MySQL query statement below
-with year_cte as (
-SELECT user_id,
-    time_stamp,
-    extract(year from time_stamp) AS years
-FROM Logins
-)
 SELECT user_id,
     max(time_stamp) as last_stamp
-FROM year_cte
-WHERE years = '2020'
+FROM Logins
+WHERE extract(year from time_stamp) = '2020'
 GROUP BY user_id
