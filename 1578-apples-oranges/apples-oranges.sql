f1ff330148b2d83d4fedaea1b10ef7fbd27449bb
Ganesh Thampi <ganeshthampi077@gmail.com>
2024-08-06 11:24:31 -0400

    Time: 355 ms (27.17%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,8 +1,10 @@
-# Write your MySQL query statement below
-
-select sale_date,sum(case when 
-               fruit = 'apples' then sold_num
-               else -1*sold_num end) as diff
-from Sales
-group by sale_date
-order by sale_date
+SELECT 
+    a.sale_date, a.sold_num-b.sold_num AS diff
+FROM 
+    (SELECT sale_date, sold_num FROM Sales WHERE fruit IN ('apples'))a
+JOIN
+    (SELECT sale_date, sold_num FROM Sales WHERE fruit IN ('oranges'))b
+ON 
+    a.sale_date = b.sale_date
+GROUP BY 1
+ORDER BY 1
