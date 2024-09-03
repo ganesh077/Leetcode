4158f852ed18ff98fd3d97edb716b7bedcf0ca98
Ganesh Thampi <ganeshthampi077@gmail.com>
2024-09-03 11:51:33 -0400

    Time: 594 ms (18.12%) | Memory: 30 MB (63.24%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -1,16 +1,27 @@
 class Solution:
+    def binary(self,prefix,remainder):
+        low = 0
+        high = len(prefix) - 1
+        while low < high:
+            mid = (low + high)//2
+            if prefix[mid] <= remainder:
+                low = mid + 1
+            else:
+                high = mid
+        return high
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         i = 0
         total = sum(chalk)
+        n = len(chalk)
+        cum_sum,temp = [0]*n,0
+        for i in range(n):
+            temp += chalk[i]
+            cum_sum[i] = temp
 
+        k = k%cum_sum[-1]
 
-        while k >= total:
-            k %= total
+        return self.binary(cum_sum,k)
         
-        while k >= chalk[i]:
-            k -= chalk[i]
-            i += 1
-            if i == len(chalk):
-                i = 0
-        return i
+        
+        
             
