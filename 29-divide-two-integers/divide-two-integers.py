cb06373c2ce6dbd61616f559cb6bc3ab7ed3a242
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-01-08 17:23:10 -0800

    Time: 7 ms (3.08%) | Memory: 17.7 MB (36.60%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.py
+++ 29-divide-two-integers/divide-two-integers.py
@@ -1,18 +1,26 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        INT_MAX, INT_MIN = 2147483647, -2147483648
+        INT_MAX, INT_MIN = 2**31-1, -2**31
 
-        if dividend == INT_MIN and divisor == -1:
+        if divisor == -1 and dividend == INT_MIN:
             return INT_MAX
-        neg = (dividend < 0) != (divisor < 0)
+
         quo = 0
-        dividend,divisor = abs(dividend),abs(divisor)
+
+        neg = (divisor < 0) != (dividend < 0)
+
+        dividend, divisor = abs(dividend), abs(divisor)
+
+        
+
         for i in range(31,-1,-1):
-            if (divisor << i) <= dividend:
+            if dividend >= (divisor << i):
                 dividend -= (divisor << i)
                 quo += (1 << i)
-
+        
         return -quo if neg else quo
+        
+         
 
 
         
