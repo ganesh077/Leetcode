ea73b0da879c2c6f3ca94592d4fabedb8ab9840a
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-02-02 14:38:00 -0800

    Time: 4 ms (100.00%) | Memory: 44.1 MB (48.09%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 ) {
+        if (x < 0 || (x !=0 && x % 10 == 0)) {
             return false;
         }
-
-        if (x == reverse(x)) {
+        int temp = x;
+        int res = 0;
+        while (temp > res) {
+            res = res * 10 + (temp%10);
+            temp = temp/10;
+        }
+        if (temp == res || temp == res/10) {
             return true;
         }
         else {
             return false;
         }
+        
 
         
     }
 
-    static int reverse(int x) {
-        int temp = x;
-        int res = 0;
-        while (temp > 0) {
-            res = res * 10 + (temp%10);
-            temp = temp/10;
-        }
-        return res;
-    }
 }
