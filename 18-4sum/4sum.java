7bfa7dd20c1d8ab72cb8a2123653e4422f38ecfe
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-02-24 17:08:13 -0800

    Time: 27 ms (17.81%) | Memory: 45.5 MB (12.93%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,26 +1,23 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
+     public List<List<Integer>> Ksum(int[] nums, long target, int k, int start) {
+       
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        int len = nums.length;
 
-        for(int i=0; i<len-3; i++) {
-            if (i>0 && nums[i] == nums[i-1]) continue;
-            for (int j = i+1; j<len-2; j++) {
-                if (j>i+1 && nums[j] == nums[j-1]) continue;
-                int left = j+1;
-                int right = len - 1;
-                long starget = (long)target-nums[i]-nums[j];
+        if (k==2) {
+            int left = start;
+            int right = nums.length - 1;
+            for (int j = start; j < nums.length ; j++) {
+                if (j>start+1 && nums[j] == nums[j-1]) continue;
                 while (left < right) {
                     long sum = (long)nums[left] + nums[right];
-                    if (sum == starget) {
-                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[left],nums[right]));
                         while(left < right && nums[left] == nums[left+1]) left++;
                         while(left < right && nums[right] == nums[right-1]) right--;
                         left++;
                         right--;
                     }
-                    else if (sum > starget) {
+                    else if (sum > target) {
                         right--;
                     }
                     else {
@@ -29,8 +26,26 @@ class Solution {
                 }
             }
         }
+        else {
+            for (int i=start; i < nums.length-(k-1); i++) {
+                if (i>start && nums[i] == nums[i-1]) continue;
+                List<List<Integer>> subList = Ksum(nums, (long)target-nums[i], k-1, i+1);
+                for (List<Integer> sub: subList) {
+                    List<Integer> temp = new ArrayList<Integer>(sub);
+                    temp.add(0,nums[i]);
+                    result.add(temp);
+                }
+            }
+            
+        }
 
         return result;
 
     }
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+         Arrays.sort(nums);
+         return Ksum(nums,target,4,0);
+    }
+   
 }
