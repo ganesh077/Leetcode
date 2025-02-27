4c9a92c226c9b6dd4e4d15d4a0fa28a2f1948885
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-02-26 22:12:06 -0800

    Time: 0 ms (100.00%) | Memory: 42.3 MB (41.83%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,28 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-         int point_m = 0;
-         int point_n = 0;
-         int k = m;
+         int point_m = m-1;
+         int point_n = n-1;
+         int point = (m+n)-1;
 
-         while (point_n < n && point_m < m) {
-            if (nums1[point_m] <= nums2[point_n]) {
-                point_m++;
+         while (point_n >= 0 && point_m >= 0) {
+            if (nums1[point_m] > nums2[point_n]) {
+                nums1[point] = nums1[point_m];
+                point_m--;
+                point--;
             }
             else {
-                k = m;
-                while (k > point_m) {
-                    nums1[k] = nums1[k-1];
-                    k--;
-                }
-                nums1[point_m] = nums2[point_n];
-                point_n++;
-                m++;
+                nums1[point] = nums2[point_n];
+                point_n--;
+                point--;
             }
          }
     
-         for (int i=point_n; i<n; i++) {
-            nums1[m] = nums2[i];
-            m++;
+         while (point_n >=0) {
+            nums1[point] = nums2[point_n];
+            point--;
+            point_n--;
          }
     }
 }
