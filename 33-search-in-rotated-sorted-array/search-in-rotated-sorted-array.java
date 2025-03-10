a337f3b6f117d1c28bec16c6e59110b952c7ffa8
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-03-10 13:03:11 -0700

    Time: 0 ms (100.00%) | Memory: 42.3 MB (28.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -31,7 +31,7 @@ class Solution {
         int right = arr.length - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] > arr[right]) {
+            if (arr[mid] >= arr[right]) {
                 left = mid + 1;
             } else {
                 right = mid;
