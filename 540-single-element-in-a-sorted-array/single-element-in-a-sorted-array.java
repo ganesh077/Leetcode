8ef0277588cf9736da208ada9044b79996a17f24
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-03-31 19:51:18 -0700

    Time: 0 ms (100.00%) | Memory: 50.3 MB (66.17%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -3,38 +3,20 @@ class Solution {
         int left = 0;
         int right = nums.length-1;
 
-        if(nums.length == 1) {
-            return nums[0];
-        }
-
         while (left < right) {
-            System.out.println(left+" "+right);
             int mid = left + (right - left)/2;
-            if(mid == 0) {
-                return nums[mid];
+
+            if(mid%2 == 1) {
+                mid--;
             }
-            if(mid == nums.length-1) {
-                return nums[mid];
-            }
+
             if(nums[mid] == nums[mid+1]) {
-                if(mid%2==0) {
-                    left = mid+2;
-                }
-                else {
-                    right = mid-1;
-                }
-            }
-            else if (nums[mid] == nums[mid-1]) {
-                if(mid%2!=0) {
-                    left = mid+1;
-                }
-                else {
-                    right = mid-2;
-                }
+                left = mid + 2;
             }
             else {
-                return nums[mid];
+                right = mid - 1;
             }
+            
         }
 
         return nums[left];
