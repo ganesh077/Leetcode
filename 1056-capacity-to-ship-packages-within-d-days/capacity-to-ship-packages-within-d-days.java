14effce2372b603b6c1272313d6a58d26a9a5170
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-04-04 16:20:11 -0700

    Time: 9 ms (98.24%) | Memory: 47 MB (92.43%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int sum = 0;
+        for(int weight: weights) {
+            sum += weight;
+        }
+        int ans = sum+1;
+        int left = sum/days;
+        int right = sum;
+
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if(cargocheck(weights,days,mid,sum)) {
+                right = mid - 1;
+                ans = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return ans-1;
+    }
+
+    public boolean cargocheck(int[] weights,int days,int current,int totalsum) {
+        int sum = 0;
+        int iterate = 0;
+        while(days > 0) {
+            while (iterate < weights.length && (sum+weights[iterate]) < current) {
+                sum += weights[iterate];
+                iterate++;
+            }
+            totalsum -= sum;
+            sum = 0;
+            days--; 
+        }
+
+        if (totalsum > 0) {
+            return false;
+        }
+        return true;
+    }
+}
