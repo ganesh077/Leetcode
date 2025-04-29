9646b2eb0d40a44670d0420484c11bd35d87f57f
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-04-29 09:05:47 -0700

    Time: 50 ms (6.04%) | Memory: 45.6 MB (21.43%) - LeetSync

--- 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.java
+++ 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.java
@@ -8,7 +8,7 @@ class Solution {
             right = Math.max(right, (stations[i] - stations[i-1]));
         }
 
-        for(int i=0; i<100 ; i++) {
+        for(int i=0; i<60 ; i++) {
             float mid = left + (right - left)/2;
 
             if(canReduce(stations, k, mid)) {
