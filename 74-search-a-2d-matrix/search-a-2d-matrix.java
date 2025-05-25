053e562d8267e4052f02260a5b43f230b255246c
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-05-25 12:15:21 -0700

    Time: 0 ms (100.00%) | Memory: 42.3 MB (39.25%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,48 +1,27 @@
 class Solution {
-    public boolean binary(int[][] matrix,int row, int target) {
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int cols = matrix[0].length;
+        int rows = matrix.length;
+
         int left = 0;
-        int right = matrix[0].length-1;
+        int right = (rows*cols)-1;
+        
 
         while(left <= right) {
             int mid = left + (right - left)/2;
-
-            if(matrix[row][mid] == target) {
+            if( matrix[mid/cols][mid%cols] == target) {
                 return true;
             }
-            else if (matrix[row][mid] > target) {
+            else if( matrix[mid/cols][mid%cols] > target) {
                 right = mid - 1;
             }
             else {
                 left = mid + 1;
             }
-        
         }
 
         return false;
-    }
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int left = 0;
-        int right = matrix.length-1;
-        int row = -1;
-
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            System.out.println(matrix[mid][matrix[0].length-1]+" "+target);
-
-            if( matrix[mid][matrix[0].length-1] >= target) {
-                row = mid;
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-
-        if(row < 0) {
-            System.out.print(row);
-            return false;
-        }
-
-        return binary(matrix,row,target);
     }
 }
