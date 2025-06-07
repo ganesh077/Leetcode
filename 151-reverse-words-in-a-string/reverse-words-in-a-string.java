2a1f5631bf9cda3f4e27a00d8bbe04053e4f6ae1
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-06-07 11:47:52 -0700

    Time: 8 ms (39.33%) | Memory: 43.3 MB (53.34%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -10,13 +10,4 @@ class Solution {
 
         return sb.toString().trim();
     } 
-
-    public String reverse(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=s.length()-1 ; i>=0; i--) {
-            sb.append(s.charAt(i));
-        }
-
-        return sb.toString();
-    }
 }
