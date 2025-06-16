df1855e461f92fb4e4a1916b0bb930cfa2671872
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-06-16 10:33:53 -0700

    Time: 1 ms (82.50%) | Memory: 43 MB (82.68%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,21 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<String>();
-        generate(ans,"",0,0,n);
+        generate(ans,new StringBuilder(),0,0,n);
         return ans;
     }
 
-    public void generate(List<String> ans, String sb, int open, int close, int n) {
+    public void generate(List<String> ans, StringBuilder sb, int open, int close, int n) {
         if(sb.length() == 2*n) {
-            ans.add(sb);
+            ans.add(sb.toString());
             return;
         }
+
         if(open < n) {
-            generate(ans,sb+'(',open+1,close,n);
+            sb.append('(');
+            generate(ans,sb,open+1,close,n);
+            sb.deleteCharAt(sb.length()-1);
         }
 
         if(open > close) {
-            generate(ans,sb+')',open,close+1,n);
+            sb.append(')');
+            generate(ans,sb,open,close+1,n);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
