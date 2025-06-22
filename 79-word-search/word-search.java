1011f008f7ea11865bbfb64836bb71e3c1d2a2ea
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-06-22 12:19:07 -0700

    Time: 142 ms (59.84%) | Memory: 41.8 MB (37.84%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -2,11 +2,26 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int r = board.length;
         int c = board[0].length;
+        boolean[] valid = new boolean[word.length()];
 
         if(word.length() > r*c) {
             return false;
         }
- 
+
+        int[] ascii = new int[256];
+
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                ascii[board[i][j]]++;
+            }
+        }
+
+        for(char i: word.toCharArray()) {
+            if(ascii[i]-- < 0) return false;
+        }
+
+        
+
         for(int i=0; i<r; i++) {
             for(int j=0; j<c; j++) {
                 if(search(board,word,0,i,j,new boolean[r][c])) return true;
