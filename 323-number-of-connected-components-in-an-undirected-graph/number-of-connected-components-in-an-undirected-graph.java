308c3f8e707d57d9f013187e63e3c1787e5ff9d1
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-07-10 10:44:03 -0700

    Time: 4 ms (62.35%) | Memory: 44.6 MB (80.64%) - LeetSync

---
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        List<List<Integer>> mylist = new  ArrayList<List<Integer>>();
+
+        for(int i=0; i<n; i++) {
+            mylist.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<edges.length; i++) {
+            mylist.get(edges[i][0]).add(edges[i][1]);
+            mylist.get(edges[i][1]).add(edges[i][0]);
+        }
+
+
+        boolean[] visit = new boolean[n];
+        int components = 0;
+        for(int i=0; i<n; i++) {
+            if(!visit[i]) {
+                dfs(i,mylist,visit);
+                components++;
+            }
+        }
+        return components;
+
+    }
+
+    public void dfs(int val, List<List<Integer>> mylist, boolean[] visit) {
+        visit[val] = true;
+        for(int i=0; i<mylist.get(val).size(); i++) {
+            if(!visit[mylist.get(val).get(i)]) {
+                dfs(mylist.get(val).get(i),mylist,visit);
+            }
+        }
+        
+    }
+}
