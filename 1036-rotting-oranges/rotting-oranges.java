b63bd2ab3997ab89e983dc724ecf6e272d3b3569
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-07-10 19:24:45 -0700

    Time: 2 ms (67.97%) | Memory: 42.6 MB (38.55%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,93 @@
+class Solution {
+    class Edge {
+        int row;
+        int col;
+
+        public Edge(int row,int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        public int getrow(){
+            return row;
+        }
+
+        public int getcol(){
+            return col;
+        }
+
+    }
+    public int orangesRotting(int[][] grid) {
+        int rowmax = grid.length-1;
+        int colmax = grid[0].length-1;
+        List<Edge> rottens = new ArrayList<>();
+        boolean nofresh = true;
+
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(grid[i][j] == 1) {
+                    nofresh = false;
+                    break;
+                }
+            }
+        }
+
+        if(nofresh) return 0;
+
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(grid[i][j] == 2) {
+                    rottens.add(new Edge(i,j));
+                }
+            }
+        }
+
+
+        Queue<Edge> q = new LinkedList<>();
+
+        for(Edge i: rottens) {
+            q.add(i);
+        }
+        int minute = -1;
+        while(!q.isEmpty()) {
+            minute++;
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                Edge curr = q.remove();
+                int currrow = curr.getrow();
+                int currcol = curr.getcol();
+
+                if(currrow < rowmax && grid[currrow+1][currcol] == 1) {
+                    grid[currrow+1][currcol] = 2;
+                    q.add(new Edge(currrow+1,currcol));
+                }
+                if(currrow > 0 && grid[currrow-1][currcol] == 1) {
+                    grid[currrow-1][currcol] = 2;
+                    q.add(new Edge(currrow-1,currcol));
+                }
+                if(currcol < colmax && grid[currrow][currcol+1] == 1) {
+                    grid[currrow][currcol+1] = 2;
+                    q.add(new Edge(currrow,currcol+1));
+                }
+                if(currcol > 0 && grid[currrow][currcol-1] == 1) {
+                    grid[currrow][currcol-1] = 2;
+                    q.add(new Edge(currrow,currcol-1));
+                }
+            }
+        }
+
+
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+
+        return minute;
+        
+    }
+
+
+}
