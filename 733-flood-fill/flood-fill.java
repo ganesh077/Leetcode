e96dd9e208a887bcd6199a285c54f5b955c9c02c
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-07-11 22:49:09 -0700

    Time: 0 ms (100.00%) | Memory: 44.6 MB (97.23%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,42 +1,26 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int original = image[sr][sc];
-        if (original == color) return image;  
-        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        int rowmax = image.length;
-        int colmax = image[0].length;
-
-        int[] origin = {sr,sc};
-
-        Queue<int[]> q = new LinkedList<>();
-        image[sr][sc] = color;
-
-        q.add(origin);
-
-        while(!q.isEmpty()) {
-            
-                int[] curr = q.poll();
-                int row = curr[0];
-                int col = curr[1];
-                //int currcolor = image[row][col];
-                
-
-                for(int[] dir:directions) {
-                    int nrow = row+dir[0];
-                    int ncol = col+dir[1];
-                    if(nrow < rowmax && nrow >= 0 && ncol < colmax && ncol >= 0 && image[nrow][ncol] == original)                  
-                        {
-                            
-                            q.offer(new int[]{nrow,ncol});   
-                            image[nrow][ncol] = color; 
-                    
-                }
-                }
-
-                
-            
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) {
+            dfs(image, sr, sc, color, newColor);
         }
-
         return image;
+    }
+    public void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r > 0) {
+                dfs(image, r - 1, c, color, newColor);
+            }
+            if (c > 0) {
+                dfs(image, r, c - 1, color, newColor);
+            }
+            if (r + 1 < image.length) {
+                dfs(image, r + 1, c, color, newColor);
+            }
+            if (c + 1 < image[0].length) {
+                dfs(image, r, c + 1, color, newColor);
+            }
+        }
     }
 }
