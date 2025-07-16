1092c9e8edb2c0584d03aed6f158a58be26720a0
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-07-15 18:07:12 -0700

    Time: 2 ms (83.09%) | Memory: 45.9 MB (18.83%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -6,20 +6,20 @@ class Solution {
 
         for(int i=0; i<rowlen; i++) {
             if(board[i][0] == 'O') {
-                bfs(board,i,0,visited,rowlen,collen);
+                dfs(board,i,0,visited,rowlen,collen);
             }
             if(board[i][collen-1] == 'O') {
-                bfs(board,i,collen-1,visited,rowlen,collen);
+                dfs(board,i,collen-1,visited,rowlen,collen);
             }
         }
 
         for(int j=0; j<collen; j++) {
             if(board[0][j] == 'O') {
-                bfs(board,0,j,visited,rowlen,collen);
+                dfs(board,0,j,visited,rowlen,collen);
             }
 
             if(board[rowlen-1][j] == 'O') {
-                bfs(board,rowlen-1,j,visited,rowlen,collen);
+                dfs(board,rowlen-1,j,visited,rowlen,collen);
             }
         }
 
@@ -32,26 +32,29 @@ class Solution {
         }
     }
 
-    public void bfs(char[][] board,int row,int col,boolean[][] visited,int rowmax,int colmax) {
-        Queue<int[]> p = new LinkedList<>();
-        p.add(new int[]{row,col});
+    public void dfs(char[][] board,int row,int col,boolean[][] visited,int rowmax,int colmax) {
+
+        if(board[row][col] != 'O' || visited[row][col]) return;
 
         visited[row][col] = true;
-        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
 
-        while(!p.isEmpty()) {
-            int size = p.size();
-            for(int i=0; i<size; i++) {
-                int[] curr = p.remove();
-                for(int[] dir:directions) {
-                    int currow = curr[0] + dir[0];
-                    int curcol = curr[1] + dir[1];
-                    if(currow >=0 && currow < rowmax && curcol >=0 && curcol < colmax && !visited[currow][curcol] && board[currow][curcol] == 'O') {
-                        p.add(new int[]{currow,curcol});
-                        visited[currow][curcol] = true;
-                    }
-                }
-            }
-        }
+        if(row+1 < rowmax) {
+            dfs(board,row+1,col,visited,rowmax,colmax);
+         }
+
+         if(row-1 >= 0) {
+            dfs(board,row-1,col,visited,rowmax,colmax);
+         }
+
+        if(col+1 < colmax) {
+            dfs(board,row,col+1,visited,rowmax,colmax);
+         }
+
+        if(col-1 >= 0) {
+            dfs(board,row,col-1,visited,rowmax,colmax);
+         }
+
+
+
     }
 }
