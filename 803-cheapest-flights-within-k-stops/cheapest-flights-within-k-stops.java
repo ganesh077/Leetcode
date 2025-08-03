8eafccb8596c2fbeec1a04f0407a49b19564b63b
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-08-03 10:50:00 -0700

    Time: 6 ms (83.18%) | Memory: 44.4 MB (56.18%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,8 +1,8 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
 
-        int[][] costArray = new int[n][k + 2];
-        for (int[] row : costArray) Arrays.fill(row, Integer.MAX_VALUE);
+        int[] costArray = new int[n];
+        Arrays.fill(costArray, Integer.MAX_VALUE);
         
 
         List<List<int[]>> adj = new ArrayList<>();
@@ -12,29 +12,26 @@ class Solution {
             adj.get(flight[0]).add(new int[]{flight[1],flight[2]});
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> Integer.compare(x[1],y[1]));
+        Queue<int[]> pq = new LinkedList<>();
 
         pq.add(new int[]{src,0,0});
-        costArray[src][0] = 0;
-
+        costArray[src] = 0;
+        int level = 0;
         while(!pq.isEmpty()) {
             int[] curr = pq.poll();
             int city = curr[0], cost = curr[1], stops = curr[2];
-
-            
-            if(city == dst) return cost;
             if(stops > k) continue;
-            
             for(int[] neigh: adj.get(city)) {
-                int nextFlights  = stops + 1;
-                if(cost+neigh[1] < costArray[neigh[0]][nextFlights]) {
-                    pq.add(new int[]{neigh[0],cost+neigh[1],nextFlights});
-                    costArray[neigh[0]][nextFlights] = cost+neigh[1];
+                
+                if(cost+neigh[1] < costArray[neigh[0]] && stops <= k) {
+                    pq.add(new int[]{neigh[0],cost+neigh[1],stops+1});
+                    costArray[neigh[0]] = cost+neigh[1];
                 }  
             }
+
         }
 
       
-        return -1;
+        return costArray[dst] == Integer.MAX_VALUE?-1:costArray[dst];
      }
 }
