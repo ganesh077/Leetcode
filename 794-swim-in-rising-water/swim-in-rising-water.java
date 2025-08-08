c15cce74733cf4b14b9c33117626b8eb8eddd38a
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-08-08 16:35:43 -0700

    Time: 96 ms (5.03%) | Memory: 45.5 MB (5.76%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,27 +1,77 @@
 class Solution {
+    class Disjoint {
+        int[] par, size;
+
+        public Disjoint(int x) {
+            par = new int[x];
+            size = new int[x];
+
+            Arrays.fill(size,1);
+            for(int i=0; i<x; i++) par[i] = i;
+        }
+
+        public int find(int x) {
+            if(par[x] == x) return x;
+            return par[x] = find(par[x]);
+        }
+
+        public boolean union(int x, int y) {
+            int leadx = find(x), leady = find(y);
+            if(leadx == leady) return false;
+
+            if(size[leadx] >= size[leady]) {
+                par[leady] = leadx;
+                size[leadx] += size[leady];
+            } 
+            else {
+                par[leadx] = leady;
+                size[leady] += size[leadx];
+            }
+
+            return true;
+        }
+    }
     public int swimInWater(int[][] grid) {
         int n = grid.length;
+        List<int[]> adj = new ArrayList<>();
+        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
         boolean[][] visited = new boolean[n][n];
-        int[][] directions = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
+        Disjoint dj = new Disjoint(n*n);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> Integer.compare(x[2],y[2]));
-        pq.add(new int[]{0,0,grid[0][0]});
-        visited[0][0] = true;
-
-        while(!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int row = cur[0], col= cur[1], w= cur[2];
-            if(row == n-1 && col == n-1) return w;
-            visited[row][col] = true;
-            for(int[] dir: directions) {
-                int nrow = row + dir[0], ncol = col+dir[1];
-                if(nrow >=0 && nrow < n && ncol >= 0 && ncol < n && !visited[nrow][ncol]) {
-                    int maxw = Math.max(w,grid[nrow][ncol]);
-                    pq.offer(new int[]{nrow,ncol,maxw});    
-                    
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                int flatmain = flat(n,i,j);
+                visited[i][j] = true;
+                for(int[] dir: directions) {
+                    int r = dir[0] + i, c = dir[1] + j;
+                    int flatneigh = flat(n,r,c);
+                    if(isValid(n,r,c) && !visited[r][c]) {
+                        int val = Math.max(grid[i][j], grid[r][c]);
+                        adj.add(new int[]{flatmain,flatneigh,val});
+                    }
                 }
             }
         }
-        return -1;
+       Collections.sort(adj,(x,y) -> Integer.compare(x[2],y[2]));
+       int max = 0;
+       for (int[] in : adj) {
+          System.out.println(Arrays.toString(in));
+          if(dj.union(in[0],in[1])) {
+            max = Math.max(max,in[2]);
+          }
+          if(dj.find(0) == dj.find(n*n-1)) return max;
+       }
+    
+        return max;
+        
+    }
+
+    public int flat(int len, int row, int col) {
+        return len*row+col;
+    }
+
+    public boolean isValid(int max,int r ,int c) {
+        if(r >= 0 && r < max && c >= 0 && c < max) return true;
+        return false;
     }
 }
