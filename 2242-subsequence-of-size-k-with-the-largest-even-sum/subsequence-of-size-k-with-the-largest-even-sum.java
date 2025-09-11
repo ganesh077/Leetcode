efef57109b4524b5d58aa2a5ed1f2c9119dc4840
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-09-10 18:59:44 -0700

    Time: 19 ms (88.89%) | Memory: 56 MB (100.00%) - LeetSync

--- 2242-subsequence-of-size-k-with-the-largest-even-sum/subsequence-of-size-k-with-the-largest-even-sum.java
+++ 2242-subsequence-of-size-k-with-the-largest-even-sum/subsequence-of-size-k-with-the-largest-even-sum.java
@@ -16,30 +16,26 @@ class Solution {
             int minkEven = -1, minKOdd = -1;
             for(int i=0; i<n-k; i++) {
                 if(nums[i]%2 == 0) {
-                    if(maxEven == - 1) maxEven = nums[i];
-                    else maxEven = Math.max(maxEven, nums[i]);
+                    maxEven = nums[i];
                 }
                 else {
-                    if(maxOdd == - 1) maxOdd = nums[i];
-                    else maxOdd = Math.max(maxOdd, nums[i]);
+                    maxOdd = nums[i];
                 }
             }
 
-            for(int i=n-k; i<n; i++) {
+            for(int i=n-1; i>=n-k; i--) {
                 if(nums[i]%2 == 0) {
-                    if(minkEven == - 1) minkEven = nums[i];
-                    else minkEven = Math.min(minkEven, nums[i]);
+                    minkEven = nums[i];
                 }
                 else {
-                    if(minKOdd == - 1) minKOdd = nums[i];
-                    else minKOdd = Math.min(minKOdd, nums[i]);
+                    minKOdd = nums[i];
                 }
             }
 
             long best = -1;
-            best = Math.max(best, (maxOdd == -1 || minkEven == -1)? - 1 : sum - minkEven + maxOdd);
+            if (maxOdd != -1 && minkEven != -1) best = Math.max(best, (sum - minkEven + maxOdd));
             
-            best = Math.max(best, (maxEven == -1 || minKOdd == -1)? - 1 : sum - minKOdd + maxEven);
+            if (maxEven != -1 && minKOdd != -1) best = Math.max(best, (sum - minKOdd + maxEven));
 
             return best;
 
